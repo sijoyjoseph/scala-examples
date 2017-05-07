35de22de425805e04ee05942b8cb0a7a003a1bac
sijoyjoseph <joseph.sijoy3@gmail.com>
2017-05-07 18:21:38 +0530

    if you want details

--- pa.scala
+++ pa.scala
@@ -1,3 +1,8 @@
 // More functional way.
 
-args.foreach(args => println(args))
+// args.foreach(args => println(args))
+
+// If you prefer to be more explicit, you can also mention the type name, but the 
+// argument protion needs to be wrapped in parantheses.
+
+args.foreach((args: String) => println(args))
