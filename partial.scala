987c3352c7b0908c0c67753b2c3a16449bd4ec3b
sijoyjoseph <joseph.sijoy3@gmail.com>
2017-05-07 18:09:37 +0530

    brevity rules

---
+++ partial.scala
@@ -0,0 +1,8 @@
+// Partially applied function
+// If a function literal consists of one statement
+// and takes a single argument, you need not explicitly name
+// and specify the argument.
+
+// args.foreach(args => println(args))
+
+args.foreach(println)
