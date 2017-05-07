275daa3ded72e6f963d6f80477d38c8cba63390e
sijoyjoseph <joseph.sijoy3@gmail.com>
2017-05-07 19:31:35 +0530

    a step away from functional

---
+++ forargs.scala
@@ -0,0 +1,9 @@
+// for loop in scala.
+
+for (arg <- args)
+  println(arg)
+
+// here arg is val not var. It cannot be reassigned.
+// Instead for each values of the args a new arg val
+// will be created and the initialized and the body
+// of the for loop will be executed.
