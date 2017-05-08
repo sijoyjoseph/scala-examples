bdc3303b9fccbf7e33b3c35ef52a227afed615ad
sijoyjoseph <joseph.sijoy3@gmail.com>
2017-05-08 19:54:35 +0530

    Using new keyword in Scala

---
+++ parameterize.scala
@@ -0,0 +1,25 @@
+// Parametrizing both type and value
+
+val greetings = new Array[String](3)
+
+greetings(0) = "Hello"
+greetings(1) = ", "
+greetings(2) = "world!"
+
+for (i <- 0 to 2)
+  print(greetings(i))
+
+println()
+
+
+// In more explicit terms
+// val greetings: Array[String] = new Array[String](3)
+//
+// Very Verbose. What scala does internally
+/* val greetings: Array[String] = new Array[String](3)
+ * greetings.update(0, "Hello")
+ * greetings.update(1, ", ")
+ * greetings.update(2, "world!")
+ *
+ * for (1 <- (1).to(2))
+ *  print(greetings.apply(i))*/
