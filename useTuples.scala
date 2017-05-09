26ed1438f8d1d90becb685a88036d1268457fc0e
sijoyjoseph <joseph.sijoy3@gmail.com>
2017-05-09 21:50:28 +0530

    Using Tuples

---
+++ useTuples.scala
@@ -0,0 +1,9 @@
+//Tuples are immutable like lists in Scala,
+//but unlike lists tuples can contain different
+//types of elements. Its very useful in returning
+//multiple objects from a method.
+
+
+val pairs = (99, "LuftBallons")
+println(pairs._1)
+println(pairs._2)
